61c9915f02ea27d87e66810d272f07e175a421b3
Depth-Hoar <44712760+Depth-Hoar@users.noreply.github.com>
2023-04-05 09:31:13 -0600

    observer pattern

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -7,18 +7,9 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
+  onAuthStateChanged,
 } from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk",
-//   authDomain: "crwn-clothing-db-98d4d.firebaseapp.com",
-//   projectId: "crwn-clothing-db-98d4d",
-//   storageBucket: "crwn-clothing-db-98d4d.appspot.com",
-//   messagingSenderId: "626766232035",
-//   appId: "1:626766232035:web:506621582dab103a4d08d6",
-// };
-
 
 const firebaseConfig = {
   apiKey: "AIzaSyB8izoFLgAG1ypDlm9LEATjSYUyRXD-l28",
@@ -88,3 +79,6 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 };
 
 export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener = (callback) =>
+  onAuthStateChanged(auth, callback);
